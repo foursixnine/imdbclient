0dbc4befd6e5ac0088c9f4a686a562dd38644b5c
Santiago Zarate <santiago@zarate.co>
2025-12-18 17:58:39 +0100

    Fix wrong header

--- internal/client/client.go
+++ internal/client/client.go
@@ -49,7 +49,7 @@ func (t *imdbClientTransport) RoundTrip(req *http.Request) (*http.Response, erro
 	r.Header.Add("Content-Type", "application/json")
 	r.Header.Add("Accept", "application/json")
 	r.Header.Add("Accept-Charset", "UTF-8")
-	r.Header.Add("UserAgent", t.UserAgent)
+	r.Header.Add("User-Agent", t.UserAgent)
 	// r.Header.Add("X-AUTH-API-KEY", t.apiKey)
 
 	return http.DefaultTransport.RoundTrip(r)
