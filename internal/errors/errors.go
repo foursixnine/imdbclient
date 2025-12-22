b495906332ba87ec92e8b43907f5ffbc4511cd07
Santiago Zarate <santiago@zarate.co>
2025-12-22 17:57:06 +0100

    Make custom error simpler

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -1,7 +1,7 @@
 package errors
 
 import (
-	// "errors"
+	// e "errors"
 	"fmt"
 	"net/http"
 )
@@ -11,14 +11,18 @@ type HTTPError struct {
 	Message string
 }
 
-func (e *HTTPError) Error() string {
-	return fmt.Sprintf("error: path or body: (%s), status code %d", e.Message, e.Code)
+func (e HTTPError) Error() string {
+	return e.Message
 }
+
+// func (e HTTPError) Unwrap() error {
+// 	return e.Message
+// }
 
 func New(code int, message string) *HTTPError {
 	return &HTTPError{
-		Message: message,
 		Code:    code,
+		Message: fmt.Sprintf("error: path or body: (%s), status code %d", message, code),
 	}
 }
 
@@ -28,4 +32,11 @@ func NotFound(message string) *HTTPError {
 
 func UnexpectedError(statusCode int, message string) *HTTPError {
 	return New(statusCode, message)
+}
+
+func (e HTTPError) Is(target error) bool {
+	if t, ok := target.(*HTTPError); ok {
+		return e.Code == t.Code && e.Message == t.Message // Or compare more fields
+	}
+	return false
 }
