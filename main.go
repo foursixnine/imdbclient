85b7dea30b7dd13b1894fd9e8eaf369d12cf7072
Santiago Zarate <santiago@zarate.co>
2025-12-25 13:39:39 +0100

    Refactor main function into smaller chunks

--- main.go
+++ main.go
@@ -21,11 +21,11 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	done := make(chan struct{})
-	var err *ce.IMDBClientApplicationError
 
-	url, parseerr := url.Parse("https://api.imdbapi.dev")
-	if parseerr != nil {
-		panic(parseerr)
+	url, err := url.Parse("https://api.imdbapi.dev")
+	if err != nil {
+		log.Printf("Error parsing api url: %v", err)
+		os.Exit(1)
 	}
 
 	options := client.ImdbClientOptions{
@@ -35,49 +35,52 @@ func main() {
 	}
 
 	imdbClient := client.New(options)
-	go func() {
-		var titles []*models.ImdbapiTitle
-
-		defer wg.Done()
-		fmt.Println("Finding results:")
-		titles, err = imdbClient.FindShowsByTitle("Stranger Things")
-		if err != nil {
-			var appErr *ce.IMDBClientApplicationError
-			if errors.As(err, &appErr) && err.AppMessage == "Search title cannot be empty" {
-				log.Printf("Title cannot be empty %v\n", err)
-				os.Exit(404)
-			}
-			log.Printf("An error has occurred: (%v)\n", err)
-			os.Exit(2)
-		}
-		close(done)
-		fmt.Println("\nDone fetching results.")
-
-		if len(titles) == 0 {
-			log.Println("No titles found")
-		}
-
-		for _, title := range titles {
-			fmt.Printf("(%s)\t-> \"%s\"\n", title.ID, title.OriginalTitle)
-		}
-	}()
-	go func() {
-		progressMarker(done)
-	}()
+	go getTitles(imdbClient, &wg, done)
+	go progressMarker(done)
 
 	wg.Wait()
 
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+}
+
+func getTitles(imdbClient *client.ImdbClient, wg *sync.WaitGroup, done chan struct{}) {
+	var titles []*models.ImdbapiTitle
+	var err *ce.IMDBClientApplicationError
+
+	defer wg.Done()
+	fmt.Println("Finding results:")
+	titles, err = imdbClient.FindShowsByTitle("Stranger Things")
 	if err != nil {
-		panic(err)
+		var appErr *ce.IMDBClientApplicationError
+		if errors.As(err, &appErr) && err.AppMessage == "Search title cannot be empty" {
+			log.Printf("Title cannot be empty %v\n", err)
+			os.Exit(404)
+		}
+		log.Printf("An error has occurred: (%v)\n", err)
+		os.Exit(2)
+	}
+	close(done)
+	fmt.Println("\nDone fetching results.")
+
+	if len(titles) == 0 {
+		log.Println("No titles found")
 	}
 
+	for _, title := range titles {
+		fmt.Printf("(%s)\t-> \"%s\"\n", title.ID, title.OriginalTitle)
+	}
 }
 
 func progressMarker(done chan struct{}) {
 	counter := 0
+	ticker := time.NewTicker(1 * time.Millisecond)
+	defer ticker.Stop()
 	for {
 		select {
-		case <-time.After(1 * time.Millisecond):
+		case <-ticker.C:
 			fmt.Print(".")
 			counter++
 			if counter%100 == 0 {
